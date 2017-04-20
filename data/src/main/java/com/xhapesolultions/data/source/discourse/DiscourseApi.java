3256e1ff3e1094888d73642bd8b0d369db8f6b92
Angelo Javonitalla <amjavonitalla@gmail.com>
2017-04-20 23:09:07 +0800

    Initial commit discourse api

---
+++ data/src/main/java/com/xhapesolultions/data/source/discourse/DiscourseApi.java
@@ -0,0 +1,31 @@
+package com.xhapesolultions.data.source.discourse;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by Angelo Javonitalla on 20/04/2017.
+ */
+
+public interface DiscourseApi {
+
+  //region Category
+  @GET("users/{user}/repos")
+
+  //endregion
+
+  //region Group
+
+  //endregion
+
+  //region Post
+
+  //endregion
+
+  //region Topic
+
+  //endregion
+
+  //region User
+
+  //endregion
+}
